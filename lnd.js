9afaf32e1fdcceebdd24492e6c34cc99c56b291f
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-12-29 14:50:54 +0100

    tidied lnd.js

--- lnd.js
+++ lnd.js
@@ -1,4 +1,4 @@
-const lndGrpc = require('./lnd-grpc')
+const LndGrpc = require('grpc-lnd')
 const through = require('through2')
 const path = require('path')
 const { EventEmitter } = require('events')
@@ -8,29 +8,13 @@ const fs = require('fs')
 process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
 
 module.exports = class Payment {
-  constructor (sellerAddress, lndRpc, nodeAddress) {
+  constructor (sellerAddress, opts) {
     this.seller = sellerAddress
-    this.settled = []
-    this.pending = []
-    this.sentPayments = []
+    this.client = LndGrpc(opts)
 
-    this.client = lndRpc
-    this.users = null
-    // this.filter = filter(this.settled, this.pending, sellerAddress, this.users)
-    this.nodeId = nodeAddress
+    this.nodeId = opts.nodeAddress
     this.invoiceStream = this.client.subscribeInvoices({})
     this.lastIndex = 0
-  }
-
-  init (cb) {
-    const self = this
-
-    this.client.getInfo({}, function (err, res) {
-      if (err) return cb(err)
-
-      self.nodeId = `${res.identity_pubkey}@${self.lndHost}`
-      cb()
-    })
   }
 
   connect (address, cb) {
@@ -232,41 +216,4 @@ function toSats (btcAmount) {
 
 function fromSats (btcAmount) {
   return btcAmount / 10 ** 8
-}
- 
-function relayToBuyer () {}
-
-function lightningRpc (opts) {
-  // load macaroon from .lnd directory
-  const macaroonPath = path.join(opts.lnddir, 'data', 'chain', 'bitcoin', opts.network, 'admin.macaroon')
-  const m = fs.readFileSync(macaroonPath)
-  const macaroon = m.toString('hex')
-
-  // build metadata credentials
-  const metadata = new grpc.Metadata()
-  metadata.add('macaroon', macaroon)
-  const macaroonCreds = grpc.credentials.createFromMetadataGenerator((_args, callback) => {
-    callback(null, metadata)
-  })
-
-  // build ssl credentials
-  const tlsCertPath = path.join(opts.lnddir, 'tls.cert')
-  const lndCert = fs.readFileSync(tlsCertPath)
-  const sslCreds = grpc.credentials.createSsl(lndCert)
-
-  // combine cert credentials and macaroon auth credentials
-  const credentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds)
-  const packageDefinition = protoLoader.loadSync(
-    opts.rpcProtoPath,
-    { keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true
-    })
-
-  // pass the credentials when creating a channel
-  const lnrpcDescriptor = grpc.loadPackageDefinition(packageDefinition)
-  const lnrpc = lnrpcDescriptor.lnrpc
-  return new lnrpc.Lightning(opts.port, credentials)
 }
